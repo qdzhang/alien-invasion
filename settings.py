1221ef896359ab967845488d9ba0cd334a1b4af8
qidongz <qdzhangcn@gmail.com>
2018-08-09 18:00:37 +0800

    Update

--- settings.py
+++ settings.py
@@ -11,3 +11,10 @@ class Settings():
 
         # 飞船的设置
         self.ship_speed_factor = 1.5
+
+        # 子弹的设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 248, 248, 255
+        self.bullets_allowed = 3 
