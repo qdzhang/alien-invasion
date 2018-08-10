ef4f1a747ba20fb9e3931fbddac19107ff6cc5b3
qidongz <qdzhangcn@gmail.com>
2018-08-10 09:48:20 +0800

    Create aliens group

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,7 +22,7 @@ def run_game():
     bullets = Group()
 
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏的主循环
     while True:
