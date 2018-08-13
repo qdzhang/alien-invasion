d29131f1c8e80f5861d484e14700a031b0ab85f6
qidongz <qdzhangcn@gmail.com>
2018-08-13 21:15:48 +0800

    Update

--- game_functions.py
+++ game_functions.py
@@ -69,6 +69,11 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
 
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+
+    
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
     # 检查是否有子弹击中了外星人
     # 如果这样， 就删除相应的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
