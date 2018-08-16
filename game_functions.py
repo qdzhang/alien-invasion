a76e5ff77a5820ff4f00136c3c4ce40b73268597
qidongz <qdzhangcn@gmail.com>
2018-08-16 11:53:40 +0800

    Update

--- game_functions.py
+++ game_functions.py
@@ -27,7 +27,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,6 +36,27 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(ai_settings, screen, stats, play_button, ship, 
+                aliens, bullets, mouse_x, mouse_y)
+
+
+def check_play_botton(ai_settings, screen, stats, play_button, 
+    mouse_x, mouse_y):
+    """在玩家单击play时开始游戏"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # 重置游戏统计信息
+        stats.reset_stats()
+        stats.game_active = True
+       
+        #清空外星人列表和子弹列表
+        aliens.empty() 
+        bullets.empty()
+
+        # 创建一群外星人，并让飞船居中
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
